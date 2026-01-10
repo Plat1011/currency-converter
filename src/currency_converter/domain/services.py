837752f1b18fce883baf56b602f392a588a4c2df
Plat <manko.platon@gmail.com>
2026-01-10 19:33:18 +0300

    Add models and convert service

---
+++ src/currency_converter/domain/services.py
@@ -0,0 +1,8 @@
+from models import Currency, Money
+
+
+def convert(money: Money, to_currency: Currency, rate: float) -> Money:
+    if money.currency.code == to_currency.code:
+        return Money(amount=money.amount, currency=to_currency)
+
+    return Money(amount=money.amount * rate, currency=to_currency)
